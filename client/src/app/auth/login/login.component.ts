dd9e6989f98c91ab7af294e01864b310c813b2df
bryryann <186111776+bryryann@users.noreply.github.com>
2025-02-04 20:45:39 -0300

    feat(client): define reactive forms for authentication

--- client/src/app/auth/login/login.component.ts
+++ client/src/app/auth/login/login.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  form = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl('')
+  });
 
+  onSubmit() {
+    const username = this.form.value.username;
+    const password = this.form.value.password;
+
+    console.log(username, password);
+
+    this.form.reset();
+  }
 }
--- client/src/app/auth/signup/signup.component.ts
+++ client/src/app/auth/signup/signup.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
+  form = new FormGroup({
+    username: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl(''),
+    confirmPassword: new FormControl('')
+  });
 
+  onSubmit() {
+    const { username, email, password, confirmPassword } = this.form.value;
+
+    console.log(`username: ${username}`);
+    console.log(`email: ${email}`);
+    console.log(`password: ${password}`);
+    console.log(`confirmPassword: ${confirmPassword}`);
+
+    this.form.reset();
+  }
 }
